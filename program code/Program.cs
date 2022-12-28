e2a1156a37a7305a2b9c86d05f0615bb2e40aec6
Wilovod <wilovod@gmail.com>
2022-12-28 10:42:19 +0700

    add program code

---
+++ program code/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+string[] arrOne = {"hello","2","world",":-)"};
+string[] arrTwo = new string[arrOne.Length-1];
+int j = 0;
+
+for (int i=0; i< arrOne.Length;i++)
+{
+    if (arrOne[i].Length <= 3)
+    {
+        arrTwo[j] = arrOne[i];
+        Console.WriteLine(arrTwo[j]);
+        j++;
+    }
+}
+
+
+
